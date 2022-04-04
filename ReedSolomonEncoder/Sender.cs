b524c1f6f7fa15031d96345bd69f6e00976ed556
Valerii Martell <valerii.martell@gmail.com>
2022-04-04 20:55:57 +0200

    Structure changed

---
+++ ReedSolomonEncoder/Sender.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReedSolomonEncoder
+{
+    class Sender
+    {
+        private int[] code = new int[15];
+
+        public Sender(int[] informationSymbols)
+        {
+            for (int i = 0; i < informationSymbols.Length; i++)
+            {
+                this.code[i] = informationSymbols[i];
+            }
+            code[informationSymbols.Length] = 0;
+            code[informationSymbols.Length + 1] = 0;
+            code[informationSymbols.Length + 2] = 0;
+            code[informationSymbols.Length + 3] = 0;
+        }
+
+        public int[] Code
+        {
+            get { return GaluaField.IDFT(this.code); }
+        }
+    }
+}
